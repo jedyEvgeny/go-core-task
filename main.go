c1577a236db91336023b4e4ac8cebcb2996d3e5b
Евгений Клюкман <kef1991@yandex.ru>
2024-11-20 18:51:43 +1000

    initial commit

--- main.go
+++ main.go
@@ -1,78 +1,28 @@
 package main
 
-import "fmt"
-
-type stringIntMap struct {
-	stringInts map[string]int
-}
+import (
+	"fmt"
+)
 
 func main() {
-	si := &stringIntMap{
-		make(map[string]int),
-	}
+	slice1 := []string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"}
+	slice2 := []string{"banana", "date", "fig"}
 
-	si.add("Первый", 1)
-	si.add("Второй", 2)
-	si.add("Третий", 3)
-	fmt.Printf("Исходные данные:\n%v\n\n", *si)
-
-	mNew := si.copy()
-
-	si.remove("Третий")
-
-	fmt.Printf("Копия:\n%v\n\n", mNew)
-	fmt.Printf("Данные после удаления:\n%v\n\n", *si)
-
-	key := "Второй"
-	found := si.isExists(key)
-	if !found {
-		fmt.Printf("Ключ `%s` не найден\n", key)
-	}
-	if found {
-		fmt.Printf("Ключ `%s` найден\n", key)
-	}
-	key = "Третий"
-	found = si.isExists(key)
-	if !found {
-		fmt.Printf("Ключ `%s` не найден\n", key)
-	}
-	if found {
-		fmt.Printf("Ключ `%s` найден\n", key)
-	}
-
-	key = "Первый"
-	val, ok := si.get(key)
-	if ok {
-		fmt.Printf("\nЭлемент с ключём %s найден и равен: %d\n",
-			key, val)
-	}
-	if !ok {
-		fmt.Printf("\nЭлемент с кллючём %s не найден", key)
-	}
+	strs := differenceSlice(slice1, slice2)
+	fmt.Println(strs)
 }
 
-func (si *stringIntMap) add(key string, val int) {
-	si.stringInts[key] = val
-}
-
-func (si *stringIntMap) remove(key string) {
-	delete(si.stringInts, key)
-}
-
-func (si *stringIntMap) copy() map[string]int {
-	m := make(map[string]int)
-	for key, val := range si.stringInts {
-		m[key] = val
+func differenceSlice(s1, s2 []string) []string {
+	uniqStrs := make(map[string]struct{})
+	for _, str := range s2 {
+		uniqStrs[str] = struct{}{}
 	}
-	return m
-}
-
-func (si *stringIntMap) isExists(key string) bool {
-	_, ok := si.stringInts[key]
-	return ok
-}
-
-func (si *stringIntMap) get(key string) (int, bool) {
-	val, ok := si.stringInts[key]
-	return val, ok
+	strs := make([]string, 0, 1)
+	for _, str := range s1 {
+		_, ok := uniqStrs[str]
+		if !ok {
+			strs = append(strs, str)
+		}
+	}
+	return strs
 }
--- main_test.go
+++ main_test.go
@@ -2,280 +2,77 @@ package main
 
 import "testing"
 
-type addTestCase struct {
-	key         string
-	val         int
-	expectedMap map[string]int
+type testDifferenceSliceCase struct {
+	s1, s2, expectedStrs []string
 }
 
-func TestAdd(t *testing.T) {
-	testCases := []addTestCase{
-		{"О", 1, map[string]int{"О": 1}},
-		{"Д", 2, map[string]int{"О": 1, "Д": 2}},
-		{"Т", 3, map[string]int{"О": 1, "Д": 2, "Т": 3}},
-		{"Ч", 4, map[string]int{"О": 1, "Д": 2, "Т": 3, "Ч": 4}},
-		{"F", 5, map[string]int{"О": 1, "Д": 2, "Т": 3, "Ч": 4, "F": 5}},
+func TestDifferenceSlice(t *testing.T) {
+	testCases := []testDifferenceSliceCase{
+		{
+			[]string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"},
+			[]string{"banana", "date", "fig"},
+			[]string{"apple", "cherry", "43", "lead", "gno1"},
+		},
+		{
+			[]string{},
+			[]string{},
+			[]string{},
+		},
+		{
+			[]string{""},
+			[]string{},
+			[]string{""},
+		},
+		{
+			[]string{},
+			[]string{""},
+			[]string{},
+		},
+		{
+			[]string{"0"},
+			[]string{""},
+			[]string{"0"},
+		},
+		{
+			[]string{""},
+			[]string{"0"},
+			[]string{""},
+		},
+		{
+			[]string{"apple", "banana", "banana"},
+			[]string{"banana"},
+			[]string{"apple"},
+		},
+		{
+			[]string{"apple", "banana"},
+			[]string{"banana", "banana"},
+			[]string{"apple"},
+		},
+		{
+			[]string{"banana", "banana", "banana"},
+			[]string{"banana"},
+			[]string{},
+		},
 	}
-	si := &stringIntMap{make(map[string]int)}
-
 	for _, el := range testCases {
-		si.add(el.key, el.val)
-		if !isEqualMaps(si.stringInts, el.expectedMap) {
-			t.Errorf("\nОжидалась карта:\n\t%v\nимеется:\n\t%v\n",
-				el.expectedMap, *si)
+		strs := differenceSlice(el.s1, el.s2)
+		if !equalSlices(strs, el.expectedStrs) {
+			t.Errorf("\nВходной слайс №1: %s\nВходной слайс №1: %s\nОжидался результат:\n\t%s\nполучили:\n\t%s\n",
+				el.s1, el.s2, el.expectedStrs, strs)
 		}
 	}
 }
 
-func isEqualMaps(m1, mExpected map[string]int) bool {
-	if len(m1) != len(mExpected) {
+func equalSlices(arr1, arrTemplate []string) bool {
+	if len(arr1) != len(arrTemplate) {
 		return false
 	}
-	for key, val := range mExpected {
-		valM1, ok := m1[key]
-		if !ok || valM1 != val {
+	//Проверка с учётом порядка элементов
+	//Если порядок не важен, можно сделать проверку через карту
+	for i := range arrTemplate {
+		if arrTemplate[i] != arr1[i] {
 			return false
 		}
 	}
 	return true
-}
-
-type testRemoveCase struct {
-	key         string
-	initMap     map[string]int
-	expextedMap map[string]int
-}
-
-func TestRevomve(t *testing.T) {
-	testCases := []testRemoveCase{
-		{
-			"F",
-			map[string]int{"О": 1, "Д": 2, "Т": 3, "Ч": 4, "F": 5},
-			map[string]int{"О": 1, "Д": 2, "Т": 3, "Ч": 4},
-		},
-		{
-			"Ч",
-			map[string]int{"О": 1, "Д": 2, "Т": 3, "Ч": 4},
-			map[string]int{"О": 1, "Д": 2, "Т": 3},
-		},
-		{
-			"Т",
-			map[string]int{"О": 1, "Д": 2, "Т": 3},
-			map[string]int{"О": 1, "Д": 2},
-		},
-		{
-			"Д",
-			map[string]int{"О": 1, "Д": 2},
-			map[string]int{"О": 1},
-		},
-		{
-			"О",
-			map[string]int{"О": 1},
-			map[string]int{},
-		},
-	}
-
-	for _, el := range testCases {
-		si := &stringIntMap{el.initMap}
-
-		si.remove(el.key)
-		if !isEqualMaps(si.stringInts, el.expextedMap) {
-			t.Errorf("\nОжидалась карта:\n\t%v\nимеется:\n\t%v\n",
-				el.expextedMap, *si)
-		}
-	}
-}
-
-type testCopyCase struct {
-	m           map[string]int
-	expectedMap map[string]int
-}
-
-func TestCopy(t *testing.T) {
-	testCases := []testCopyCase{
-		{
-			map[string]int{"О": 1, "Д": 2, "Т": 3, "Ч": 4},
-			map[string]int{"О": 1, "Д": 2, "Т": 3, "Ч": 4},
-		},
-		{
-			map[string]int{},
-			map[string]int{},
-		},
-		{
-			map[string]int{"": 0},
-			map[string]int{"": 0},
-		},
-		{
-			map[string]int{"-": -500000000000},
-			map[string]int{"-": -500000000000},
-		},
-		{
-			map[string]int{"漢字": 0},
-			map[string]int{"漢字": 0},
-		},
-	}
-
-	key := "testKey"
-	var val int
-	for _, el := range testCases {
-		si := &stringIntMap{el.m}
-		newM := si.copy()
-
-		if !isEqualMaps(si.stringInts, newM) {
-			t.Errorf("\nОжидалась карта:\n\t%v\nимеется:\n\t%v\n",
-				el.expectedMap, newM)
-		}
-
-		si.stringInts[key] = val
-		if isEqualMaps(si.stringInts, newM) {
-			t.Errorf("\nКарта изменяется влед за базовой картой.\nБазовая карта:\n\t%v\nИмеется:\n\t%v\nДолжно быть:\n\t%v\n",
-				si.stringInts, newM, el.expectedMap)
-		}
-
-		if len(newM) == 0 {
-			continue
-		}
-
-		var randKey string
-		for key := range newM {
-			randKey = key
-		}
-
-		newM[randKey] = val
-		if isEqualMaps(si.stringInts, newM) {
-			t.Errorf("\nКарта изменяется влед за копией.\nБазовая карта:\n\t%v\nИзменённая копия:\n\t%v\nДолжно быть:\n\t%v\n",
-				si.stringInts, newM, el.expectedMap)
-		}
-	}
-}
-
-type testIsExistsCase struct {
-	key    string
-	m      map[string]int
-	status bool
-}
-
-func TestIsExists(t *testing.T) {
-	testCases := []testIsExistsCase{
-		{
-			"О",
-			map[string]int{"О": 1, "Д": 2, "Т": 3, "Ч": 4},
-			true,
-		},
-		{
-			"1",
-			map[string]int{},
-			false,
-		},
-		{
-			"",
-			map[string]int{},
-			false,
-		},
-		{
-			"",
-			map[string]int{"": 0},
-			true,
-		},
-		{
-			"0",
-			map[string]int{"0": 0},
-			true,
-		},
-		{
-			"0",
-			map[string]int{"0": 5, "50": 0},
-			true,
-		},
-		{
-			"-",
-			map[string]int{"-": -500000000000},
-			true,
-		},
-		{
-			"漢字",
-			map[string]int{"漢字": 0},
-			true,
-		},
-	}
-	for _, el := range testCases {
-		si := &stringIntMap{el.m}
-		found := si.isExists(el.key)
-		if found != el.status {
-			t.Errorf("\nКлюч `%s`:\nожидалось:\n\t%v\nполучено:\n\t%v\n\t[карта: %v]",
-				el.key, el.status, found, el.m)
-		}
-	}
-}
-
-type testGetCase struct {
-	key    string
-	m      map[string]int
-	val    int
-	status bool
-}
-
-func TestGet(t *testing.T) {
-	testCases := []testGetCase{
-		{
-			"О",
-			map[string]int{"О": 1, "Д": 2, "Т": 3, "Ч": 4},
-			1,
-			true,
-		},
-		{
-			"1",
-			map[string]int{},
-			0,
-			false,
-		},
-		{
-			"",
-			map[string]int{},
-			0,
-			false,
-		},
-		{
-			"",
-			map[string]int{"": 0},
-			0,
-			true,
-		},
-		{
-			"0",
-			map[string]int{"0": 0},
-			0,
-			true,
-		},
-		{
-			"0",
-			map[string]int{"0": 5, "50": 0},
-			5,
-			true,
-		},
-		{
-			"-",
-			map[string]int{"-": -500000000000},
-			-500000000000,
-			true,
-		},
-		{
-			"漢字",
-			map[string]int{"漢字": 0},
-			0,
-			true,
-		},
-	}
-	for _, el := range testCases {
-		si := &stringIntMap{el.m}
-		val, ok := si.get(el.key)
-		if ok != el.status {
-			t.Errorf("\nКлюч `%s`:\nожидалось:\n\t%v\nполучено:\n\t%v\n\t[карта: %v]",
-				el.key, el.status, ok, el.m)
-		}
-		if val != el.val {
-			t.Errorf("\nКлюч `%s`:\nожидалось:\n\t%d\nполучено:\n\t%d\n\t[карта: %v]",
-				el.key, val, el.val, el.m)
-		}
-	}
 }
