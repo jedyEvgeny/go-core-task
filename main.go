6b9cfe9ed2feed8fcd5bb6c39c043fa05d59732e
Евгений Клюкман <kef1991@yandex.ru>
2024-11-25 17:24:34 +1000

    task-9 initial commit

--- main.go
+++ main.go
@@ -2,73 +2,43 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
-type semaphor struct {
-	semaphor chan struct{}
-	count    int
-	doneCh   chan struct{}
-}
-
-const maxGorutines = 2
-
 func main() {
-	s := new(maxGorutines)
+	numbers := make(chan uint8)
+	squares := make(chan float64)
 
-	for i := 0; i < 5; i++ {
-		s.add(1)
-		go func(i int) {
-			defer s.done()
-			printLoad(i)
-			fmt.Printf("\rЗапуск сервиса №%d завершён!\n", i+1)
-		}(i)
-	}
+	go producer(numbers)
+	go cube(numbers, squares)
+	go consumer(squares)
 
-	s.wait()
-	fmt.Println("Все сервисы запущены")
+	time.Sleep(5 * time.Second)
 }
 
-func new(size int) *semaphor {
-	return &semaphor{
-		semaphor: make(chan struct{}, size),
-		count:    0,
-		doneCh:   make(chan struct{}),
+func producer(numbers chan<- uint8) {
+	defer close(numbers)
+	for {
+		num := rand.Intn(100)
+		fmt.Printf("Исходное число: %v\t", num)
+		numbers <- uint8(num)
+		time.Sleep(350 * time.Millisecond)
 	}
 }
 
-func (s *semaphor) add(delta int) {
-	s.count += delta
-	for i := 0; i < delta; i++ {
-		s.semaphor <- struct{}{}
+func cube(numbers <-chan uint8, squares chan<- float64) {
+	defer close(squares)
+	for num := range numbers {
+		numF := float64(num)
+		squared := numF * numF * numF
+		fmt.Printf("Отправленное число: %v\t", squared)
+		squares <- squared
 	}
 }
 
-func (s *semaphor) done() {
-	<-s.semaphor
-	s.count--
-	if s.count == 0 {
-		close(s.doneCh)
-	}
-}
-
-func (s *semaphor) wait() {
-	<-s.doneCh
-}
-
-func printLoad(i int) {
-	symbols := []string{"/", "|", "-", "\\"}
-	delay := 900
-	if i%2 != 0 {
-		delay = 1300
-	}
-
-	end := time.Now().Add(time.Duration(delay) * time.Millisecond)
-
-	for time.Now().Before(end) {
-		for _, el := range symbols {
-			fmt.Printf("\rЗапускаем сервис: %s", el)
-			time.Sleep(150 * time.Millisecond)
-		}
+func consumer(squares <-chan float64) {
+	for squared := range squares {
+		fmt.Printf("Получено: %v\n", squared)
 	}
 }
